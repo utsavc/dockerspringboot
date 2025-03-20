f7e7f89317115f282cd43288597f774a16356bbb
utsavc <utsavmahato0@gmail.com>
2025-03-20 16:51:32 +1000

    cleared unnecessay files

--- src/main/java/com/utsav/dockerspringboot/repository/BookRepository.java
+++ src/main/java/com/utsav/dockerspringboot/repository/BookRepository.java
@@ -1,6 +1,5 @@
 package com.utsav.dockerspringboot.repository;
 
-import com.utsav.dockerspringboot.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
